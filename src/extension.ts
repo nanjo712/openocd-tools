2699aa9b3df400f69eeb68969b4197550d823338
woshiren <yejiacong@outlook.com>
2024-07-27 18:23:52 +0800

    fix the debug and add feature of reusing terminal

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { assert } from 'console';
 import { stat } from 'fs';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "openocd-tools" is now active!');
 	const cfgFileName = context.workspaceState.get("openocd-tools.cfg", "").split("/").pop();
 	const targetFileName = context.workspaceState.get("openocd-tools.target", "").split("/").pop();
+	const svdFileName = context.workspaceState.get("openocd-tools.svd", "").split("/").pop();	
+
+	let DebugTerminal: vscode.Terminal | undefined = undefined;
+	let FlashTerminal: vscode.Terminal | undefined = undefined;
 
 	const statusBarChooseCfg = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
 	statusBarChooseCfg.text = "$(file-directory) Choose cfg";
@@ -26,6 +23,12 @@ export function activate(context: vscode.ExtensionContext) {
 	statusBarChooseTarget.tooltip = "Target file for OpenOCD: [" + targetFileName + "]";
 	statusBarChooseTarget.show();
 
+	const statusBarChooseSVD = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	statusBarChooseSVD.text = "$(file-directory) Choose SVD";
+	statusBarChooseSVD.command = "openocd-tools.chooseSVD";
+	statusBarChooseSVD.tooltip = "SVD file for OpenOCD: [" + svdFileName + "]";
+	statusBarChooseSVD.show();
+
 	const statusBarFlash = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
 	statusBarFlash.text = "$(triangle-right) Flash";
 	statusBarFlash.command = "openocd-tools.flash";
@@ -36,9 +39,6 @@ export function activate(context: vscode.ExtensionContext) {
 	statusBarDebug.command = "openocd-tools.debug";
 	statusBarDebug.show();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposableForFlash = vscode.commands.registerCommand('openocd-tools.flash', () => {
 		const cfgFile = context.workspaceState.get("openocd-tools.cfg", "");
 		const targetFile = context.workspaceState.get("openocd-tools.target", "");
@@ -55,9 +55,11 @@ export function activate(context: vscode.ExtensionContext) {
 				vscode.window.showErrorMessage("Build failed");
 				return;
 			}
-			const terminal = vscode.window.createTerminal("OpenOCD Flash");
-			terminal.show();
-			terminal.sendText(`openocd -f ${cfgFile} -c "program ${targetFile} verify reset exit"`);
+			if (!FlashTerminal) {
+				FlashTerminal = vscode.window.createTerminal("OpenOCD Flash");
+			}
+			FlashTerminal.show();
+			FlashTerminal.sendText(`openocd -f ${cfgFile} -c "program ${targetFile} verify reset exit"`);
 		});
 	});
 	context.subscriptions.push(disposableForFlash);	
@@ -65,6 +67,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposableForDebug = vscode.commands.registerCommand('openocd-tools.debug', () => {
 		const cfgFile = context.workspaceState.get("openocd-tools.cfg", "");
 		const targetFile = context.workspaceState.get("openocd-tools.target", "");
+		const svdFile = context.workspaceState.get("openocd-tools.svd", "");
 		if (cfgFile === '') {
 			vscode.window.showErrorMessage("Please choose a cfg file first");
 			return;
@@ -73,14 +76,50 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage("Please choose a target file first");
 			return;
 		}
+		if (svdFile === '') {
+			vscode.window.showInformationMessage("No SVD file chosen, debugging without SVD file");
+		}
 		vscode.commands.executeCommand("cmake.build").then((ret) => {
 			if (ret) {
 				vscode.window.showErrorMessage("Build failed");
 				return;
 			}
-			const terminal = vscode.window.createTerminal("OpenOCD Debug");
-			terminal.show();
-			terminal.sendText(`openocd -f ${cfgFile} -c "gdb_port 3333"`);
+			if (!DebugTerminal) {
+				DebugTerminal = vscode.window.createTerminal("OpenOCD Debug");
+			}
+			DebugTerminal.show();
+			DebugTerminal.sendText(`openocd -f ${cfgFile} -c "gdb_port 3333" -c "tcl_port disabled" -c "telnet_port 4444"`);
+			const launchConfig = {
+				name: "OpenOCD Debug",
+            	type: "cppdbg",
+            	request: "launch",
+	            program: targetFile,
+    	        svdPath: svdFile,
+        	    args: [],
+            	stopAtEntry: false,
+	            cwd: "${workspaceFolder}",
+    	        environment: [],
+        	    externalConsole: false,
+            	MIMode: "gdb",
+            	setupCommands: [
+                	{
+	                    "description": "Enable pretty-printing for gdb",
+    	                "text": "-enable-pretty-printing",
+        	            "ignoreFailures": true
+            	    },
+	                {
+    	                "description": "Set Disassembly Flavor to Intel",
+        	            "text": "-gdb-set disassembly-flavor intel",
+            	        "ignoreFailures": true
+	                },
+    	            {
+        	            "description": "Set remote target to port 3333",
+            	        "text": "target remote :3333",
+                	    "ignoreFailures": false
+                	}
+            	]
+			};
+			vscode.debug.startDebugging(vscode.workspace.workspaceFolders![0], launchConfig);
 		});
 	});
 	context.subscriptions.push(disposableForDebug);
@@ -129,6 +168,38 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	});
 	context.subscriptions.push(disposableForChooseTarget);
+
+	const disposableForChooseSVD = vscode.commands.registerCommand('openocd-tools.chooseSVD', () => {
+		vscode.window.showOpenDialog({
+			canSelectFiles: true,
+			canSelectFolders: false,
+			canSelectMany: false,
+			// set default path to workspace's build directory
+			defaultUri: vscode.workspace.workspaceFolders ? vscode.Uri.file(vscode.workspace.workspaceFolders[0].uri.fsPath + "/build") : undefined,
+			filters: {
+				"System View Description": ["svd"],
+				"All Files": ["*"]
+			}
+		}).then((fileUri) => {
+			if (fileUri) {
+				const filePath = fileUri[0].fsPath;
+				const svdFile = filePath;
+				const fileName = filePath.split("/").pop();
+				statusBarChooseTarget.tooltip = "SVD file for OpenOCD: [" + fileName + "]";
+				context.workspaceState.update("openocd-tools.svd", svdFile);
+			}
+		});
+	});
+	context.subscriptions.push(disposableForChooseSVD);
+
+	const terminateDebug = vscode.debug.onDidTerminateDebugSession((session) => {
+		if (session.name === "OpenOCD Debug") {
+			if (DebugTerminal) {
+				DebugTerminal.sendText("\x03");
+			}
+		}
+	});
+	context.subscriptions.push(terminateDebug);
 }
 
 // This method is called when your extension is deactivated
